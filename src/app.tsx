7f80f837b16e514a97897f6da7a49c390de3b953
Marius Fagerland <mariusfagerland@yahoo.no>
2024-10-19 11:08:03 +0200

    create first input field

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ export function App() {
     <div>
       <h1>Idle calc</h1>
       <p>hello</p>
+      <label for="desiredCredit">Enter desire base credits</label>
+      <input name="desiredCredit" id="desiredCredit" />
     </div>
   );
 }
